2d669b8b4695877564815bf02d28742945dfa228
nikitaxgusev <Elgolf@mail.ru>
2017-11-13 01:25:18 +0300

    For Kate

--- bot.py
+++ bot.py
@@ -1,33 +1,21 @@
-# -*- coding: utf-8 -*-
-import config
 import telebot
+import config
 from telebot import types
-
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(func=lambda message: True,content_types=['document'])
-def handle_docs_photo(message):
-    try:
-        chat_id = message.chat.id
+@bot.message_handler(regexp="/КатяКолосова")
+def handle_message(message):
+    chat_id=message.chat.id
+    mess=bot.send_message(chat_id,"Привет,Кать.Хочешь я тебе кое-что скажу?")
+    bot.register_next_step_handler(mess, HelloKatya)
 
-        file_info = bot.get_file(message.document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-
-        src = 'B:/' + message.document.file_name;
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
-
-        bot.reply_to(message, "Saved that.")
-    except Exception as e:
-        bot.reply_to(message, e)
-
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
-
+def HelloKatya(message):
+    if message.text == "Да":
+        bot.send_message(message.chat.id,'Я тебя немножечко люблю:)')
+    else:
+        bot.send_message(
+            message.chat.id,
+            'Ну..Ладно, пока')
 
 if __name__ == '__main__':
-
-     bot.polling(none_stop=True)
-
+    bot.polling(none_stop=True)
